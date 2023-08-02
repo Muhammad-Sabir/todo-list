5d9303e849a58a511e6955f85b7ef6293ed03ae9
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-08-02 17:54:39 +0500

    Add completed feature

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './Task.module.css';
 
 const Task = (props) => {
+	const [isCompleted, setIsCompleted] = useState(false);
+
 	const onDeleteItemHandler = () => {
 		const newTasks = props.tasks.filter(
 			(currTask) => props.task.id !== currTask.id
@@ -11,9 +15,46 @@ const Task = (props) => {
 		props.setTasks(newTasks);
 	};
 
+	const onCheckboxChangeHandler = () => {
+		if (!isCompleted) {
+			const newTasks = props.tasks.filter(
+				(currTask) => props.task.id !== currTask.id
+			);
+
+			props.setTasks([...newTasks, props.task]);
+		} else {
+			const newTasks = props.tasks.filter(
+				(currTask) => props.task.id !== currTask.id
+			);
+
+			props.setTasks([props.task, ...newTasks]);
+		}
+
+		setIsCompleted(!isCompleted);
+	};
+
 	return (
-		<li onClick={onDeleteItemHandler} className={styles['task']}>
-			{props.task.statement}
+		<li
+			className={`${styles['task']} ${
+				isCompleted ? styles['checked'] : ''
+			}`}
+		>
+			<p className={`${styles['task__statement']}`}>
+				{props.task.statement}
+			</p>
+			<div className={styles['task__controls']}>
+				<input
+					className={styles['task__controls-checkbox']}
+					type="checkbox"
+					onChange={onCheckboxChangeHandler}
+				/>
+				<FontAwesomeIcon
+					className={styles['task__controls-trash']}
+					onClick={onDeleteItemHandler}
+					icon={faTrash}
+					style={{ color: 'white' }}
+				/>
+			</div>
 		</li>
 	);
 };
